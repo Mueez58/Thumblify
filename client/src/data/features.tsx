ac32333ba51692eafd59497b50524b1523f6d514
Mueez58 <mueez7200@gmail.com>
2026-09-01 14:09:08 +0500

    Thumblify

---
+++ client/src/data/features.tsx
@@ -0,0 +1,21 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Our AI analyzes video contant to suggest the most clickable concepts.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye-Catching Designs",
+        description: "Genrate Vibrant, high-contrast thumbnails that stand out in the feed",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Get fully layerd designs you can tweek to perfection if needed",
+    },
+    
+  
+];
